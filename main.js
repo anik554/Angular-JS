e2b5aee80fc3bf58ccc2d620cb2a16bc710da7a2
anik924 <anikimtiaz924@gmail.com>
2024-01-24 00:27:10 +0600

    concate string

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log("Hello Anik")
+
+num1 = 30;
+num2 = 50;
+resultAdd = num1+num2;
+resultSub = num2 - num1;
+console.log("Addition number : "  +  resultAdd + "Subtraction number :" + resultSub);
